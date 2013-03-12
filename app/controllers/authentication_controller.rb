15c1d210ecae37495a8fe46623a5b3966bc68689
samuelrails <samuels410@gmail.com>
2013-03-12 10:03:33 +0530

    oauth login fix gravatar image change

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,10 +1,18 @@
 class AuthenticationController < ApplicationController
-  
+
   def create
     auth = request.env["omniauth.auth"]
 
     # Try to find authentication first
     authentication = Authentication.find_by_provider_and_uid(auth['provider'], auth['uid'])
+
+    if authentication
+      # Authentication found, sign the user in.
+      flash[:sucess] = "Welcome. #{authentication.user.name}"
+      sign_in_and_redirect(:user, authentication.user)
+
+      #login_and_redirect_user(authentication.user)
+    else
       # Authentication not found, thus a new user.
       email=auth['info']['email']
       check_user=User.find_by_email(email)
@@ -16,14 +24,14 @@ class AuthenticationController < ApplicationController
         user = User.new
         user.apply_omniauth(auth)
         if user.save(:validate => false)
-          flash[:notice] = "Account created and signed in successfully."
+          flash.now[:notice] = "Account created and signed in successfully."
           sign_in_and_redirect(:user, user)
-        
+          #login_and_redirect_user(user)
         else
-          flash[:error] = "Error while creating a user account. Please try again."
+          flash.now[:error] = "Error while creating a user account. Please try again."
           redirect_to root_url
         end
       end
     end
- 
+  end
 end
